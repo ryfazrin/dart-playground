887b1cd288120024b3c74c4399358baf3f4d4483
ryfazrin <riansera515@gmail.com>
2021-09-07 09:29:49 +0800

    update main collection

--- collections/bin/collections.dart
+++ collections/bin/collections.dart
@@ -1,41 +1,15 @@
+import 'list.dart';
+import 'map.dart';
+import 'set.dart';
+
 void main(List<String> arguments) {
 
-  // List
-  List<int> numberList = [1, 2, 3, 4, 5];
+  print("====== List ======");
+  myList();
 
-  for(int i = 0; i < numberList.length; i++){
-    print(numberList[i]);
-  }
+  print("====== Set =======");
+  mySet();
 
-  print('');
-
-  // Dynamic List
-  var dynamicList = [1, 'Ryan Pazrin', true];
-
-  dynamicList.forEach((element) {
-    print(element);
-  });
-
-  dynamicList.add('Dart');
-  print(dynamicList.last);
-
-  /**
-   * Spread Operator (...)
-   */
-  var favorites = ['Seafood', 'Salad', 'Nugget', 'Soup'];
-  var others = ['Cakes', 'Pie', 'Donut'];
-
-  // tanpa Spread Operator
-  var allFavorites = [favorites, others];
-  print(allFavorites);
-
-  // dengan Spread Operator
-  var allFavoritesSO = [...favorites, ...others];
-  print(allFavoritesSO);
-
-  // list bernilai null dengan null-aware spread operator (...?)
-  var others2; // null list
-  var allFavNull = [...favorites, ...?others2];
-  print(allFavNull);
-
+  print("====== Map =======");
+  myMap();
 }
