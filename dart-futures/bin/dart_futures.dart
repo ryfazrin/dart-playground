0c30d5849a18992cd51929925f78642773f63989
ryfazrin <riansera515@gmail.com>
2021-09-08 13:40:29 +0800

    dart futures completed with data

--- dart-futures/bin/dart_futures.dart
+++ dart-futures/bin/dart_futures.dart
@@ -1,7 +1,20 @@
 void main(List<String> arguments) {
+
   final myFuture = Future(() {
     print('Creating the future');
     return 12;
   });
   print('main() done');
+
+  // Completed with data
+  getOrder().then((value) {
+    print('You Order $value');
+  });
+}
+
+// Uncompleted
+Future<String> getOrder() {
+  return Future.delayed(Duration(seconds: 3), () {
+    return 'Coffee Boba';
+  });
 }
