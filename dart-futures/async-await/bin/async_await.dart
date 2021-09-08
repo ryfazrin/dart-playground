594b10f87be5ecbf42536f1e378ef5751df898a1
ryfazrin <riansera515@gmail.com>
2021-09-08 13:59:28 +0800

    async-await with try-catch

--- dart-futures/async-await/bin/async_await.dart
+++ dart-futures/async-await/bin/async_await.dart
@@ -1,13 +1,17 @@
 void main(List<String> arguments) async { // <= tambahkan keyword async
   print('Getting your order...');
-  var order = await getOrder();
-  print('You order: $order');
+  try {
+    var order = await getOrder();
+    print('You order: $order');
+  } catch(error) {
+    print('Sorry. $error');
+  }
 }
 
 // Uncompleted
 Future<String> getOrder() {
   return Future.delayed(Duration(seconds: 3), () {
-    var isStockAvailable = true;
+    var isStockAvailable = false;
     if (isStockAvailable) {
       return 'Coffee Boba';
     } else {
